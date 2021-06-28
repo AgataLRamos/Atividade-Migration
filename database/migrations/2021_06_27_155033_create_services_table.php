6d773de033fb920e33483fb639b9644066f55651
AgataLRamos <agata.silva20@etec.sp.gov.br>
2021-06-28 13:01:31 -0300

    migration

---
+++ database/migrations/2021_06_27_155033_create_services_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id()->primary;
+            $table->timestamps();
+            $table->string('descricao');
+            $table-> float ('cost',6,2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
